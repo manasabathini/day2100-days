985c47e55d8a5faedaaf5b698e1bf4a52e930c26
manasabathini <33703157+manasabathini@users.noreply.github.com>
2023-04-26 04:49:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hello!")
+yourName = input(" What is name?")
+
+favoriteFood = input("What is your FavoriteFood?")
+
+favoriteMusic = input("What is your FavoriteMusic?")
+
+print("you are ")
+print(yourName)
+print()
+
+print("You're probably hungry for ")
+print(favoriteFood)
+
+print("your favorite music is ")
+print(favoriteMusic)
+
+print("Have a great day!")
